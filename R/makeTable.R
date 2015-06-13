ea47ed8615cd9b6d81920433a7ab9c6e9708bfb8
jasenfinch <jsf9@aber.ac.uk>
2015-06-13 10:22:29 +0100

    output sheet complete

--- R/makeTable.R
+++ R/makeTable.R
@@ -1,92 +1,67 @@
 makeTable <-
   function(annot_info){
-     bins <- names(x)
-  for (i in 1:length(x)){
-    current.mass <- x[[i]]
-    acc.mass <- current.mass[[1]]
-    cors <- current.mass[[2]]
-    cors <- cors[[1]]
-    mfs <- current.mass[[3]]
-    isodists <- current.mass[[4]]
-    sum_meas_iso <- current.mass[[6]]
-    mzeddb <- current.mass[[7]]
-    ## calculate size of mfs and isotope distribuions portion
-    if(length(isodists)>0 & !is.null(names(isodists))){
-      iso.mat <- matrix(ncol=10)
-      for (y in 1:nrow(mfs)){
-        if(mfs[y,3] %in% names(isodists)){
-        	posi <- grep(mfs[y,3],names(isodists))
-        	iso.dist.mat <- isodists[[posi]]
-        	dim.mat <- dim(iso.dist.mat)
-        	if (dim.mat > 4){
-          	iso.mat.1 <-matrix(ncol=10,nrow=4)
-          	iso.mat.1[1,1:6] <- as.matrix(mfs[y,])
-          	iso.mat.1[1:4,7:10] <- iso.dist.mat[2:5,]
-          	iso.mat <- rbind(iso.mat,iso.mat.1)
-        	} else {
-        		iso.mat.1 <-matrix(ncol=10,nrow=nrow(isodists[[posi]]))
-          	iso.mat.1[1,1:6] <- as.matrix(mfs[y,])
-          	iso.mat.1[1:nrow(isodists[[posi]]),7:10] <- isodists[[posi]]
-          	iso.mat <- rbind(iso.mat,iso.mat.1)
-        	}
-        } else {
-          mat <- matrix(ncol=10)
-          mat[1,1:6] <- as.matrix(mfs[y,])
-          iso.mat <- rbind(iso.mat,mat)
-        }        
-      }
-      iso.mat <- iso.mat[-1,]
-      isod.len <- nrow(iso.mat)
-    } else {
-      iso.len <- 1
-      isod.len <- 0
-      if (!mfs=="No MF hits"){
-      	iso.mat <- matrix(nrow=nrow(mfs),ncol=10)
-      	iso.mat[1:nrow(mfs),1:6] <- as.matrix(mfs)
+    # combine iso and mf tables if present
+    MF.iso <- list()
+    if("Molecular Formulas" %in% names(annot_info) & "Theoretical Isotope Distributions" %in% names(annot_info)){
+      if (nrow(annot_info[["Molecular Formulas"]])>0 & !is.null(annot_info[["Theoretical Isotope Distributions"]])){
+        MF.tab <- annot_info[["Molecular Formulas"]]
+        MF.tab <- matrix(apply(MF.tab,2,as.character),ncol=ncol(MF.tab))
+        colnames(MF.tab) <- colnames(annot_info[["Molecular Formulas"]])
+        Iso.tab <- annot_info[["Theoretical Isotope Distributions"]]
+        for (i in 1:nrow(MF.tab)){
+          if(MF.tab[i,3] %in% names(Iso.tab)){
+            iso.cur <- Iso.tab[[which(names(Iso.tab)==MF.tab[i,3])]]
+          } else {
+            iso.cur <- matrix(ncol=4,nrow=1)
+          }
+          colnames(iso.cur) <- c("m.z.1","Prob","rel.Int","Isotope")
+          MF.tab.cur <- matrix(ncol=ncol(MF.tab),nrow=nrow(iso.cur)-1)
+          colnames(MF.tab.cur) <- colnames(MF.tab)
+          MF.tab.cur <- rbind(as.character(MF.tab[i,]),MF.tab.cur)
+          MF.iso.tab <- cbind(MF.tab.cur,iso.cur)
+          MF.iso[[i]] <- MF.iso.tab
+        }
+        MF.iso <- ldply(MF.iso,data.frame)
+        annot_info$`Molecular Formulas` <- MF.iso
+        annot_info <- annot_info[-which(names(annot_info) == "Theoretical Isotope Distributions")]
       } else {
-        iso.mat <- matrix("",nrow=1,ncol=10)
+        MF.iso <- matrix(ncol=10,nrow=1)
+        annot_info$`Molecular Formulas` <- MF.iso
+        annot_info <- annot_info[-which(names(annot_info) == "Theoretical Isotope Distributions")]
       }
     }
-    num.rows <- c(nrow(cors),nrow(mfs),nrow(mzeddb))
-    max.rows <- max(num.rows)
-    if (isod.len > max.rows){
-    	n.rows <- isod.len
-    } else {
-    	n.rows <- max.rows
+    # rename columns for each table
+    for (i in names(annot_info)){
+      if (i == "Accurate m/z"){
+        colnames(annot_info[[i]]) <- c("Bin","Accurate Mass","Intensity")
+      }
+      if (i == "Correlation Analysis"){
+        colnames(annot_info[[i]]) <- c("m/z","Coefficient","Difference","Relation_to","Relation_from")
+      }
+      if (i == "Molecular Formulas"){
+        colnames(annot_info[[i]]) <- c( "m/z","Original MF","Clean MF","Theoretical m/z","RDB",
+                                 "PPM Error","m/z","Probability","Relative Intensity","Isotope")
+      }
+      if (i == "Putative Ionisation Product"){
+        colnames(annot_info[[i]]) <- c("m/z","Theoretical m/z","MF","Name","Adduct","PPM Error")
+      }
     }
-    curr.mat <- matrix(ncol=29,nrow=n.rows)
-  	if(nrow(curr.mat)==0){
-  		curr.mat <- rbind(matrix(nrow=1,ncol=29))
-  	}
-    curr.mat[1,1] <- bins[i] 
-    curr.mat[1,2] <- acc.mass
-    if(length(cors)>1){
-    	cors[,2] <- round(as.numeric(as.character(cors[,2])),3)
-    	curr.mat[1:nrow(cors),3:7] <- as.matrix(cors)
+    # calculate number of rows needed for table
+    row <- list()
+    for (i in 1:length(annot_info)){
+        row[[i]] <- dim(annot_info[[i]])[1]
     }
-	  if (length(iso.mat>0)){
-    	curr.mat[1:nrow(iso.mat),8:17] <- iso.mat
-	  }
-    ###### Add measured isodists here ######
-    if(length(sum_meas_iso)>1){
-    	sum_meas_iso[,3:6] <- round(as.numeric(as.character(sum_meas_iso[,3:6])),3)
-    	curr.mat[1,19:23] <- as.matrix(sum_meas_iso[,2:6])
-    	curr.mat[1,18] <- "Carbon"
-    }
-      	if (class(mzeddb)=="character" & mzeddb != "No MZedDB hits"){
-    	mzeddb <- matrix(mzeddb,ncol=6)
-    }
-    if(!is.null(nrow(mzeddb))){
-    	mzeddb[,6] <- round(as.numeric(as.character(mzeddb[,6])),2)
-    	curr.mat[1:nrow(mzeddb),24:29] <- as.matrix(mzeddb)
-    }
-    annot_all <- rbind(annot_all,curr.mat)
-  }
-  annot_all[1,] <- c("Bin","Accurate Mass","m/z","Correlation","Difference","Relation_to","Relation_from",
-  									 "m/z","Original MF","Clean MF","Theoretical m/z","No. Isotopes",
-  									 "PPM Error","m/z","Probability","Relative Intensity","Isotope","Element","Number","Mean","Standard Deviation",
-  									 "Standard Error","95% Conf Int","m/z","Theoretical m/z","MF","Name","Adduct","PPM Error")
-  title <- c("Bin","Accurate Mass",rep("Correlations",5),rep("MFs",6),rep("Theoretical Isotope Distributions",4),rep("Measured Isotope Distirbutions",6),rep("MZedDB Hits",6))
-  annot_all <- rbind(title,annot_all)
-  annot_all[is.na(annot_all)] <- ""
+    names(row) <- names(annot_info)
+    row <- max(unlist(row))
+    # expand each table to the max row number
+    annot_info <- lapply(annot_info, function(x,row){
+      row.add <- row - nrow(x)
+      new.tab <- matrix(ncol=ncol(x),nrow=row.add)
+      colnames(new.tab) <- colnames(x)
+      new.tab <- rbind(x,new.tab)
+      return(new.tab)
+    },row=row)
+    # combine each table
+    annot_table <- do.call("cbind", annot_info)
+    return(annot_table)
   }

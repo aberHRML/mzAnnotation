6607168c267575c8f893c0d41b0fc5ac4192d234
jasenfinch <jsf9@aber.ac.uk>
2016-11-11 10:40:19 +0000

    replaced filterPIP

---
+++ R/filterPIP.R
@@ -0,0 +1,15 @@
+#' filterPIP
+
+filterPIP <- 
+  function(res){
+    patterns <- c("ic acid","keto","<i>","</i>","(R)-","(S)-","-L-","L-","(+)-",
+                  "cis,","cis-","trans-","&beta;-","-D-",
+                  "D-","D.","&gamma;-","-n-","N-","&alpha;-","&alpha;","&beta;")
+    replacements <- c("ate","oxo","","","","","","","","","","","","","","","","","","","","")
+    for(i in 1:length(patterns)){
+      res[,2] <- sapply(res[,2],gsub,pattern=patterns[i],replacement=replacements[i],fixed=TRUE)
+    }
+    res[,2] <- tolower(res[,2])
+    res <- res[!duplicated(res[,2]),]
+    return(res)
+  }

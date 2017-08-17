c8ca3a04288d3fa4d059b77b3400f768c8df9420
Jasen <jsf9@aber.ac.uk>
2017-08-17 07:53:02 +0100

    removed available functions

--- R/availableAdducts.R
+++
@@ -1,9 +0,0 @@
-#' availableAdducts
-#' @description returns adducts rules available in mzAnnotation
-#' @importFrom tibble as_tibble
-#' @export
-
-availableAdducts <- function(){
-  return(as_tibble(MZedDB$ADDUCT_FORMATION_RULES))
-}
-
--- R/availableIsotopes.R
+++
@@ -1,9 +0,0 @@
-#' availableIsotopes
-#' @description returns isotopes available in mzAnnotation
-#' @importFrom tibble as_tibble
-#' @export
-
-availableIsotopes <- function(){
-  return(as_tibble(MZedDB$ISOTOPE_RULES))
-}
-

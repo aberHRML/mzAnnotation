5eab63497198a2a120f2c7b3dbf298ff81cac364
jasenfinch <jsf9@aber.ac.uk>
2017-08-17 09:17:00 +0100

    added calcMZ

---
+++ R/calcMZ.R
@@ -0,0 +1,31 @@
+#' calcM
+#' @description calculate an m/z for a given M, adduct, isotope and transformation
+#' @param M M for which to calculate an m/z
+#' @param adduct adduct to apply
+#' @param isotope isotope to apply
+#' @param transformation transformation to apply
+#' @param adducts adduct table containing available adduct rules. Defaults to table returned by \code{availableAdducts()}.
+#' @param isotopes isotope table containing available isotope rules. Defaults to table returned by \code{availableIsotopes()}.
+#' @param transformations transformations table containing available transformations rules. Defaults to table returned by \code{availableTransformations()}.
+#' @importFrom dplyr filter
+#' @examples calcMZ(116.05182,adduct = '[M+H]1+',isotope = 'C13',transformation = 'M - [O] + NH2]')
+#' @export
+
+calcMZ <- function(M, adduct = '[M+H]1+', isotope = NA, transformation = NA, adducts = mzAnnotation::Adducts, isotopes = mzAnnotation::Isotopes, transformations = mzAnnotation::Transformations){
+  
+  if (!is.na(transformation)) {
+    transformRule <- filter(transformations, `MF Change` == transformation)
+    M <- M + transformRule$Difference
+  }
+  
+  if (!is.na(isotope)) {
+    isoRule <- filter(isotopes,Isotope == isotope)
+    M <- M + isoRule$`Mass Difference`
+  }
+  
+  addRule <- filter(adducts,Name == adduct)
+  
+  mz <- (M * addRule$xM) / addRule$Charge + addRule$Add
+  
+  return(round(mz,5))
+}

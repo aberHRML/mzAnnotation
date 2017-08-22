98ff83b740b380c2564f4a1f22829e768fd1ee1d
jasenfinch <jsf9@aber.ac.uk>
2017-08-23 00:27:39 +0100

    queryPIP now uses applyIonisationRule

--- R/queryPIP.R
+++ R/queryPIP.R
@@ -1,12 +1,12 @@
 #' @importFrom purrr map
 #' @importFrom magrittr %>%
-#' @importFrom dplyr mutate select
+#' @importFrom dplyr mutate select rename
 
 queryPIP <- function(mz, ppm, add, iso = NA, adducts = mzAnnotation::Adducts, isotopes = mzAnnotation::Isotopes, DB = mzAnnotation::MZedDB, filter = T){
   # Retrieve database entries
   elementFreq <- DB$ELementFrequencies
   metaboliteRules <- DB$Rules
-  DB <- DB$DB
+  DataBase <- DB$DB
   
   # Calculate the mz range over which to search based on ppm error
   mass <- ppmRange(mz,ppm)
@@ -15,11 +15,12 @@ queryPIP <- function(mz, ppm, add, iso = NA, adducts = mzAnnotation::Adducts, is
   mass <- map(mass,calcM,adduct = add,isotope = iso)
   
   # Filter database entries based mz range
-  DB <- filter(DB, `Accurate Mass` > mass$lower & `Accurate Mass` < mass$upper)
+  DataBase <- DataBase %>%
+    filter(`Accurate Mass` > mass$lower & `Accurate Mass` < mass$upper)
   
   # If an isotope has been selected; filter the database entries based on the isotope rules
   if (!is.na(iso)) {
-    elementFreq <- elementFreq[elementFreq$ID %in% DB$ID,]
+    elementFreq <- elementFreq[elementFreq$ID %in% DataBase$ID,]
     C <- elementFreq[,'C']
     O <- elementFreq[,'O']
     Cl <- elementFreq[,'Cl']
@@ -27,33 +28,30 @@ queryPIP <- function(mz, ppm, add, iso = NA, adducts = mzAnnotation::Adducts, is
     S <- elementFreq[,'S']
     
     isotopes <- filter(isotopes,Isotope == iso)
-    DB <- filter(DB,eval(parse(text = isotopes$Rule)))
+    DataBase <- DataBase %>%
+      filter(eval(parse(text = isotopes$Rule)))
   }
   
   # Filter the database based on the adduct formation rules
-  metaboliteRules <- filter(metaboliteRules, ID %in% DB$ID)
-  Nch <- metaboliteRules[,"Nch"]
-  Nacc <- metaboliteRules[,"Nacc"]
-  Ndon <- metaboliteRules[,"Ndon"]
-  Nnhh <- metaboliteRules[,"Nnhh"]
-  Noh <-  metaboliteRules[,"Noh"]
-  Ncooh <- metaboliteRules[,"Ncooh"]
-  Ncoo <-  metaboliteRules[,"Ncoo"]
+  if (nrow(DataBase) > 0) {
+    metaboliteRules <- metaboliteRules %>% 
+      filter(ID %in% DataBase$ID) %>%
+      rowwise() %>%
+      mutate(True = applyIonisationRule(ID,add,adducts = adducts,DB = DB)) %>%
+      filter(True == T)
+    
+    DataBase <- DataBase %>%
+      filter(ID %in% metaboliteRules$ID) %>% 
+      filterPIP() %>%
+      mutate(Adduct = add, 
+             Isotope = iso, 
+             `Theoretical m/z` = calcMZ(`Accurate Mass`,add,iso),
+             `PPM Error` = ppmError(mz,`Theoretical m/z`)
+      ) %>%
+      select(ID,Name,MF,`Accurate Mass`,`Smile 1`,Adduct:`PPM Error`) %>%
+      mutate(`PPM Error` = round(`PPM Error`,5)) %>%
+      rename(Smile = `Smile 1`)
+  }
   
-  adducts <- filter(adducts,Name == add)
-  
-  DB <- filter(DB,eval(parse(text = adducts$Rule))) %>% 
-    filterPIP() %>%
-    mutate(Adduct = add, 
-           Isotope = iso, 
-           `Theoretical m/z` = calcMZ(`Accurate Mass`,add,iso),
-           `PPM Error` = ppmError(mz,`Theoretical m/z`)
-    ) %>%
-    select(ID,Name,MF,`Accurate Mass`,`Smile 1`,Adduct:`PPM Error`)
-  
-  DB$`PPM Error` <- round(DB$`PPM Error`,5)
-  
-  colnames(DB)[5] <- 'Smile'
-  
-  return(DB)
+  return(DataBase)
 }

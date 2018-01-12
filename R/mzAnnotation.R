8901ebb91875d63ebea5eb01c5fad19a9513b381
jasenfinch <jsf9@aber.ac.uk>
2018-01-12 15:47:45 +0000

    added globalVariables to remove check note for undefined variables

--- R/mzAnnotation.R
+++ R/mzAnnotation.R
@@ -3,4 +3,8 @@
 #' @useDynLib mzAnnotation
 #' @importFrom Rcpp evalCpp
 
-NULL
+globalVariables(c('Name','ID','MF','Accurate Mass','Smile 1','Adduct',
+                  'PPM Error','RelativeAbundance','Element','AtomicMass',
+                  'Frequency','Isotope','MF Change','Probability',
+                  'Relative Abundance','C','S','Elements','Error',
+                  'Theoretical m/z','True','desc','m/z'))

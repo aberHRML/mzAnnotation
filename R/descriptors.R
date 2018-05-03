0dfb85f03c26002cee8058a584f260782225c8a8
jasenfinch <jsf9@aber.ac.uk>
2018-05-03 17:57:58 +0100

    tidied descriptors function for scalability

--- R/descriptors.R
+++ R/descriptors.R
@@ -10,80 +10,70 @@
 #'     \item{TotalCharge} The total overall charge
 #' }
 #'
-#' @importFrom rcdk parse.smiles convert.implicit.to.explicit eval.desc get.total.charge
-#' @importFrom ChemmineOB forEachMol smartsSearch_OB
+#' @importFrom ChemmineOB forEachMol smartsSearch_OB prop_OB
+#' @importFrom parallel makeCluster parLapply stopCluster
 #' @export
 #' @examples
 #' data(aminoAcids)
 #' descriptors(aminoAcids$SMILE)
 
-descriptors <- function(smiles)
-{
-  smi <- map(smiles,~{
-    parse.smiles(.) %>%
-      .[[1]]
-    })
+descriptors <- function(smiles,nCores = 1,clusterType = 'FORK',verbose = T){
+  if (verbose == T) {
+    cat(length(smiles),'SMILEs\n')
+  }
   
-  desc <- tibble(
-    CHNOPS = '',
-    TotalCharge = map_int(smi,~{
-      s <- .
-      convert.implicit.to.explicit(s)
-      get.total.charge(s)
-    }),
-    HBD = map_int(smi,~{
-      s <- .
-      convert.implicit.to.explicit(s)
-      eval.desc(
-        s,
-        'org.openscience.cdk.qsar.descriptors.molecular.HBondDonorCountDescriptor'
-      )[[1]]
-    }),
-    HBA = map_int(smi,~{
-      s <- .
-      convert.implicit.to.explicit(s)
-      eval.desc(
-        s,
-        'org.openscience.cdk.qsar.descriptors.molecular.HBondAcceptorCountDescriptor'
-      )[[1]]
-    }),
-    AcidGroups = map_int(smi,~{
-      s <- .
-      convert.implicit.to.explicit(s)
-      eval.desc(
-        s,
-        'org.openscience.cdk.qsar.descriptors.molecular.AcidicGroupCountDescriptor'
-      )[[1]]
-    }),
-    BaseGroups = map_int(smi,~{
-      s <- .
-      convert.implicit.to.explicit(s)
-      eval.desc(
-        s,
-        'org.openscience.cdk.qsar.descriptors.molecular.BasicGroupCountDescriptor'
-      )[[1]]
-    }),
-    Nnhh = map_int(smiles,~{
-      forEachMol("SMILES",.,identity) %>%
-      smartsSearch_OB("[NX3;H2]")
-    }),
-    Noh = map_int(smiles,~{
-      forEachMol("SMILES",.,identity) %>%
-        smartsSearch_OB("[OX2H]")
-    }),
-    Ncooh = map_int(smiles,~{
-      forEachMol("SMILES",.,identity) %>%
-        smartsSearch_OB("[CX3](=O)[OX2H1]")
-    }),
-    Ncoo = map_int(smiles,~{
-      forEachMol("SMILES",.,identity) %>%
-        smartsSearch_OB("[CX3](=O)[OX1H0-]")
-    })
-  ) %>%
-    bind_cols(smilesToMFs(smiles) %>% select(MF),
-              smilesToAccurateMasses(smiles) %>% select(AccurateMass)) %>%
-    mutate(SMILE = smiles) %>%
-    select(SMILE,MF,AccurateMass,CHNOPS:TotalCharge)
+  getProp <- function(smile){
+    forEachMol("SMILES",smile,identity) %>%
+      prop_OB() %>%
+      as_tibble()
+  }
+  
+  if (verbose == T) {
+    cat('\nGenerating properties')
+  }
+  
+  if (nCores > 1) {
+    clus <- makeCluster(nCores,clusterType)
+    prop <- parLapply(clus,smiles,getProp)
+    stopCluster(clus)
+  } else {
+    prop <-  map(smiles,getProp)  
+  }
+  
+  prop <- prop %>%
+    bind_rows()
+  
+  if (verbose == T) {
+    cat('\nGenerating functional descriptors')
+  }
+  
+  Fgroups <- tibble(Name = c('NHH',
+                             'OH',
+                             'COOH',
+                             'COO'),
+                    String = c("[NX3;H2]",
+                               "[OX2H]",
+                               "[CX3](=O)[OX2H1]",
+                               "[CX3](=O)[OX1H0-]")
+  )
+  
+  
+  groups <- Fgroups %>%
+    split(1:nrow(Fgroups)) %>%
+    map(~{
+      cat('\n',.$Name)
+      string <- .$String
+      g <- map_int(smiles,~{
+        forEachMol("SMILES",.,identity) %>%
+          smartsSearch_OB("[NX3;H2]")
+      }) %>%
+        as_tibble()
+      names(g) <- .$Name
+      return(g)
+    }) %>%
+    bind_cols()
+  
+  desc <- bind_cols(prop,groups)
   
   return(desc)
 }

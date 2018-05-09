6e1d56b6d7c007bef9e87b739198b63adfeb1a9e
jasenfinch <jsf9@aber.ac.uk>
2018-05-09 15:11:25 +0100

    removed MF and EXACT_MASS columns from aminoAcids table and added ACCESSION_ID column

--- R/aminoAcids.R
+++ R/aminoAcids.R
@@ -2,12 +2,11 @@
 #' @description Chemical structures of the essential amino acids 
 #' @format a tibble containing 20 rows and 6 columns
 #' \describe{
+#' \item{ACCESSION_ID}
 #' \item{NAME}{}
 #' \item{InChi}{}
 #' \item{InChiKey}{}
 #' \item{SMILE}{}
-#' \item{MF}{}
-#' \item{EXACT_MASS}{}
 #' }
 
 'aminoAcids'

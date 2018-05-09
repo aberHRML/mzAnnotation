8a47bfe2b7d6425b40b3c73fb84a20c9782ffa97
jasenfinch <jsf9@aber.ac.uk>
2018-05-09 14:34:20 +0100

    fixed check notes and changed version to 1.3.0

--- R/mzAnnotation.R
+++ R/mzAnnotation.R
@@ -8,4 +8,7 @@ globalVariables(c('Name','ID','MF','Accurate Mass','Smile 1','Adduct',
                   'PPM Error','RelativeAbundance','Element','AtomicMass',
                   'Frequency','Isotope','MF Change','Probability',
                   'Relative Abundance','C','S','Elements','Error',
-                  'Theoretical m/z','True','desc','m/z','Adducts'))
+                  'Theoretical m/z','True','desc','m/z','Adducts',
+                  'Negative Charge','Positive Charge','SMILE','Total Charge',
+                  'NHH','COO'
+                  ))

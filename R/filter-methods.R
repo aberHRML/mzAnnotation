fe9b60041b736978017739200fa5b67b6b63dde4
jasenfinch <jsf9@aber.ac.uk>
2018-05-10 17:05:57 +0100

    added filter methods

---
+++ R/filter-methods.R
@@ -0,0 +1,37 @@
+
+filterMR <- function(db,lower,upper){
+  desc <- db@descriptors
+  desc <- desc %>%
+    filter(Accurate_Mass > lower & Accurate_Mass < upper)
+  acc <- db@accessions
+  acc <- acc %>%
+    filter(SMILE %in% desc$SMILE)
+  db@descriptors <- desc
+  db@accessions <- acc
+  return(db)
+}
+
+
+filterIR <- function(db,rule){
+  ef <- elementFrequencies(db)
+  ef <- ef %>%
+    filter(eval(parse(text = rule)))
+  db@descriptors <- db@descriptors %>%
+    filter(ACCESSION_ID %in% ef$ACCESSION_ID)
+  db@accessions <- db@accessions %>%
+    filter(ACCESSION_ID %in% ef$ACCESSION_ID)
+  return(db)
+}
+
+
+filterIP <- function(db,rule){
+  desc <- db@descriptors
+  desc <- desc %>%
+    filter(eval(parse(text = rule)))
+  acc <- db@accessions
+  acc <- acc %>%
+    filter(SMILE %in% desc$SMILE)
+  db@descriptors <- desc
+  db@accessions <- acc
+  return(db)
+}

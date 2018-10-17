0ab17fd270838b0698f56187e0c62132e51c78be
jasenfinch <jsf9@aber.ac.uk>
2018-10-17 17:21:32 +0100

    fixed test errors

--- tests/testthat/test-calcTools.R
+++ tests/testthat/test-calcTools.R
@@ -7,11 +7,11 @@ test_that('calcAccurateMass works',{
 })
 
 test_that('calcM works',{
-  M <- calcM(118.08626,adduct = '[M+H]1+',isotope = '13C',transformation = 'M - [O] + NH2]')
+  M <- calcM(118.08626,adduct = '[M+H]1+',isotope = '13C',transformation = 'M - [O] + [NH2]')
   expect_true(M == 116.05182)
 })
 
 test_that('calcMZ works',{
-  mz <- calcMZ(116.05182,adduct = '[M+H]1+',isotope = '13C',transformation = 'M - [O] + NH2]')
+  mz <- calcMZ(116.05182,adduct = '[M+H]1+',isotope = '13C',transformation = 'M - [O] + [NH2]')
   expect_true(mz == 118.08626)
 })

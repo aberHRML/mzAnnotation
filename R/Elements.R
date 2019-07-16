fb4835d8d7efe16b0f8ca06426affef50fd45bd2
jasenfinch <jsf9@aber.ac.uk>
2019-07-16 13:23:56 +0100

    placed data documentation into data script

--- R/Elements.R
+++
@@ -1,5 +0,0 @@
-#' Elements
-#' @description Elemental information
-#' @format A tibble containing 31 rows and 5 columns.
-
-'Elements'
---
+++ R/data.R
@@ -0,0 +1,51 @@
+#' Adducts
+#' @description Formation rules for electrospray ionisation adducts
+#' @format A tibble containing 58 rows and 6 columns
+#' \describe{
+#' \item{Name}{Adduct name}
+#' \item{Charge}{Charge added}
+#' \item{xM}{Number of M needed}
+#' \item{Add}{Mass value change}
+#' \item{Nelec}{Electron change}
+#' \item{AddAt}{pseudo formula of the atoms to be added to the molecular formula of one M}
+#' \item{RemAt}{pseudo formula of the atoms to be removed to the molecular formula of one M}
+#' \item{AddEx}{pseudo formula of the atoms to be added to the final ionisation product molecular formula}
+#' \item{RemEx}{pseudo formula of the atoms to be removed to the final ionisation product molecular formula}
+#' \item{Rule}{Structural rule for formation}
+#' }
+
+'Adducts'
+
+#' Elements
+#' @description Elemental information
+#' @format A tibble containing 31 rows and 5 columns.
+
+'Elements'
+
+#' Isotopes
+#' @description Isotope rules
+#' @format A tibble containing 8 rows and 3 columns.
+#' \describe{
+#' \item{Isotope}{Isotope name}
+#' \item{Mass Difference}{Isotopic mass difference}
+#' \item{Rule}{Elemental composition rules}
+#' }
+
+'Isotopes'
+
+#' Transformations
+#' @description Transformation rules
+#' @format A tibble containing 12 rows and 9 columns.
+#' \describe{
+#' \item{Name}{Transformation name}
+#' \item{MF Change}{Molecular formula change}
+#' \item{Difference}{Mass difference}
+#' \item{C}{Change in carbon atoms}
+#' \item{H}{Change in hydrogen atoms}
+#' \item{O}{Change in oxygen atoms}
+#' \item{N}{Change in nitrogen atoms}
+#' \item{P}{Change in phosphorus atoms}
+#' \item{S}{Change in sulphur atoms}
+#' }
+
+'Transformations'

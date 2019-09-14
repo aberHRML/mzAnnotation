2f40b7d46b5124ad47ceedbde1de8618a2c2c0b9
wilsontom <wilsontom89@gmail.com>
2019-09-14 17:27:05 +0100

    add safeConvert funciton (convert function wrapped in callr so random segfaults return NULL

---
+++ R/safeConvert.R
@@ -0,0 +1,23 @@
+#' Safe Convert
+#' @description safely convert between SMILES and Inchi and to InchiKey using callr to catch potential segfaults
+#' @param input a valid SMILE or Inchi
+#' @param inputType either "smiles" or "inchi", denoting the input type
+#' @param outputType either "smiles", "inchi" or "inchikey", denoting the output type
+#' @examples
+#' safeConvert(aminoAcids$SMILE[1],'smiles','inchi')
+#' @export
+
+safeConvert <- function(input, inputType, outputType) {
+  output <- tryCatch(
+    r(
+      function(x)
+        convert(x, inputType, outputType),
+      args = list(x),
+      error = 'stack'
+    ),
+    error = function(e)
+      return(e$error)
+  )
+  
+  return(output)
+}

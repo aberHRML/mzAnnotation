c53d9e12d0c71d644e7b17f0504ee3854b6df3bc
jasenfinch <jsf9@aber.ac.uk>
2020-08-26 22:44:19 +0100

    added ionisationProducts function

---
+++ R/ionisationProducts.R
@@ -0,0 +1,22 @@
+#' Ionisation Products
+#' @description Calculate ionisation products for a given SMILES.
+#' @param SMILES a valid SMILES string
+#' @param adductTable table of adduct rules. Defaults to adducts()
+#' @examples 
+#' ionisationProducts(aminoAcids$SMILES[1])
+#' @importFrom dplyr ungroup
+#' @export
+
+ionisationProducts <- function(SMILES,adductTable = adducts()){
+  desc <- descriptors(SMILES)
+  
+  adductTable %>%
+    select(Name,Rule) %>%
+    bind_cols(desc) %>%
+    rowwise() %>%
+    mutate(Possible = eval(parse(text = Rule)),
+           `m/z` = calcMZ(Accurate_Mass,Name,adductTable = adductTable),
+           MF = adductTransformMF(MF,Name,Adducts = adductTable)) %>%
+    select(Adduct = Name,`m/z`,MF,Possible) %>%
+    ungroup()
+}

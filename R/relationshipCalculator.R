cab38f88d1dd37d94da5e38e284b34db84129ff9
jasenfinch <jsf9@aber.ac.uk>
2021-04-29 13:20:01 +0100

    moved calculateMs into relationshipCalculator file

--- R/relationshipCalculator.R
+++ R/relationshipCalculator.R
@@ -60,3 +60,26 @@ relationshipCalculator <- function(mz, limit = 0.001, modes = NULL, adducts = c(
     as_tibble()
   return(combinations)
 }
+
+#' @importFrom tibble tibble rowid_to_column
+#' @importFrom dplyr rowwise
+
+calculateMs <- function(mz,add,iso,trans,adductTable = adducts(), isotopeTable = isotopes(), transformationTable = transformations()) {
+  M <- map(trans,~{
+    t <- .
+    t <- map(iso,~{
+      i <- .
+      if (i == 'NA') {i <- NA}
+      if (t == 'NA') {t <- NA}
+      i <- tibble(`m/z` = mz, Adduct = add, Isotope = i, Transformation = t) %>%
+        rowwise() %>%
+        mutate(M = calcM(`m/z`,adduct = Adduct,isotope = Isotope,transformation = Transformation,adductTable = adductTable,isotopeTable = isotopeTable,transformationTable = transformationTable))
+      return(i)
+    })
+    t <- bind_rows(t)
+    return(t)
+  })
+  M <- bind_rows(M)
+  M <- rowid_to_column(M, 'ID')
+  return(M)
+}

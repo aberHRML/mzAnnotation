f0e1fe4d2714020e40b601aeaaebd9ec40515e4d
jasenfinch <jsf9@aber.ac.uk>
2021-11-22 13:23:29 +0000

    added suitableMaxElements()

--- R/generateMF.R
+++ R/generateMF.R
@@ -1,3 +1,28 @@
+#' Calculate suitable maximum elemental frequencies
+#' @description Calculate maximum elemental frequencies for a given mass which are suitable for molecular formula generation.
+#' @param mass accurate mass
+#' @return named numeric vector of element frequencies
+#' @examples
+#' suitableMaxElements(342.11621)
+#' @export
+
+suitableMaxElements <- function(mass){
+  carb <- round(mass/12)
+  Hs <- round(carb * 2)
+  NO <- round(carb / 2)
+  PS <- round(carb / 4)
+  
+  maxi <- c(C = carb,
+            H = Hs,
+            N = NO,
+            O = NO,
+            P = PS,
+            S = PS)
+  
+  return(maxi)
+}
+
+
 #' Molecular formula generation
 #' @description Molecular formula generation
 #' @param mass accurate mass
@@ -18,7 +43,12 @@
 #'                   element_max = c(C = 12,H = 22,N = 0,
 #'                                 O = 11,P = 0,S = 0))
 
-generateMF <- function(mass, ppm = 1, charge = 0, validation = TRUE, element_max = c(C = 12,H = 22,N = 0,O = 11,P = 0,S = 0), element_min = c(C = 0,H = 0,N = 0,O = 0,P = 0,S = 0)){
+generateMF <- function(mass, 
+                       ppm = 1, 
+                       charge = 0, 
+                       validation = TRUE, 
+                       element_max = suitableMaxElements(mass), 
+                       element_min = c(C = 0,H = 0,N = 0,O = 0,P = 0,S = 0)){
 
     comp_max <- c(C = 0,iC = 0,H = 0,iH = 0,N = 0,iN = 0,O = 0,iO = 0,F = 0 ,Na = 0,
              Si = 0,P = 0,S = 0,Cl = 0,iCl = 0,Br = 0,iBr = 0,K = 0,iK = 0)

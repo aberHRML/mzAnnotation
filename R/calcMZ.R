dfe7e39449ebbd5eda27f162654d159e3f783b28
jasenfinch <jsf9@aber.ac.uk>
2021-11-22 14:29:22 +0000

    fixed calcMZ()

--- R/calcMZ.R
+++ R/calcMZ.R
@@ -4,24 +4,32 @@
 #' @param adduct adduct to apply
 #' @param isotope isotope to apply
 #' @param transformation transformation to apply
-#' @param adductTable adduct table containing available adduct rules. Defaults to \code{adducts()}.
-#' @param isotopeTable isotope table containing available isotope rules. Defaults to \code{isotopes()}.
-#' @param transformationTable transformations table containing available transformations rules. Defaults to \code{transformations()}.
+#' @param adduct_rules_table adduct table containing available adduct rules. Defaults to \code{adducts()}.
+#' @param isotope_rules_table isotope table containing available isotope rules. Defaults to \code{isotopes()}.
+#' @param transformation_rules_table transformations table containing available transformations rules. Defaults to \code{transformations()}.
 #' @importFrom dplyr filter
 #' @examples calcMZ(116.05182,adduct = '[M+H]1+',isotope = '13C',transformation = 'M - [O] + NH2]')
 #' @export
 
-calcMZ <- function(M, adduct = '[M+H]1+', isotope = NA, transformation = NA, adductTable = adducts(), isotopeTable = isotopes(), transformationTable = transformations()){
+calcMZ <- function(M, 
+                   adduct = '[M+H]1+', 
+                   isotope = NA, 
+                   transformation = NA, 
+                   adduct_rules_table = adduct_rules(), 
+                   isotope_rules_table = isotope_rules(), 
+                   transformation_rules_table = transformation_rules()){
   
-  addRule <- filter(adductTable,Name == adduct)
+  addRule <- filter(adduct_rules_table,Name == adduct)
   
   if (!is.na(transformation)) {
-    transformRule <- filter(transformationTable, `MF Change` == transformation)
+    transformRule <- filter(transformation_rules_table, 
+                            `MF Change` == transformation)
     M <- M + transformRule$Difference
   }
   
   if (!is.na(isotope)) {
-    isoRule <- filter(isotopeTable,Isotope == isotope)
+    isoRule <- filter(isotope_rules_table,
+                      Isotope == isotope)
     mz <- (M * addRule$xM) + isoRule$`Mass Difference`
   } else {
     mz <- (M * addRule$xM)

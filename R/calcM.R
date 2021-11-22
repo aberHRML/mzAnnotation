ff6f0e025d6ca7dc7390995f1fa4580c58e9a587
jasenfinch <jsf9@aber.ac.uk>
2021-11-22 14:09:31 +0000

    fixed calcM

--- R/calcM.R
+++ R/calcM.R
@@ -4,28 +4,34 @@
 #' @param adduct adduct to apply
 #' @param isotope isotope to apply
 #' @param transformation transformation to apply
-#' @param adductTable adduct table containing available adduct rules. Defaults to \code{Adducts}.
-#' @param isotopeTable isotope table containing available isotope rules. Defaults to \code{Isotopes}.
-#' @param transformationTable transformations table containing available transformations rules. Defaults to \code{Transformations}.
+#' @param adduct_rules_table tibble containing available adduct formation rules. Defaults to `adduct_rules()`.
+#' @param isotope_rules_table tibble containing available isotopic rules. Defaults to `isotope_rules()`.
+#' @param transformation_rules_table tibble containing available transformation rules. Defaults to `transformation_rules()`.
 #' @importFrom dplyr filter
 #' @examples calcM(118.08626,adduct = '[M+H]1+',isotope = '13C',transformation = 'M - [O] + NH2]')
 #' @export
 
-calcM <- function(mz, adduct = '[M+H]1+', isotope = NA, transformation = NA, adductTable = adducts(), isotopeTable = isotopes(), transformationTable = transformations()){
+calcM <- function(mz, 
+                  adduct = '[M+H]1+', 
+                  isotope = NA, 
+                  transformation = NA, 
+                  adduct_rules_table = adduct_rules(), 
+                  isotope_rules_table = isotope_rules(), 
+                  transformation_rules_table = transformation_rules()){
   
-  addRule <- filter(adductTable,Name == adduct)
+  addRule <- filter(adduct_rules_table,Name == adduct)
   
   M <- ((mz - addRule$Add) * addRule$Charge) 
   
   if (!is.na(isotope)) {
-    isoRule <- filter(isotopeTable,Isotope == isotope)
+    isoRule <- filter(isotope_rules_table,Isotope == isotope)
     M <- (M - isoRule$`Mass Difference`)
   }
   
   M <- M / addRule$xM
   
   if (!is.na(transformation)) {
-    transformRule <- filter(transformationTable, `MF Change` == transformation)
+    transformRule <- filter(transformation_rules_table, `MF Change` == transformation)
     M <- M - transformRule$Difference
   }
   return(round(M,5))

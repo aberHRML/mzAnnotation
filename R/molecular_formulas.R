7416139fb557dc118d39c33862530e80c7203775
jasenfinch <jsf9@aber.ac.uk>
2022-05-23 17:41:20 +0100

    move adductTransformMF and transformMF to molecular formulas file

--- R/molecular_formulas.R
+++ R/molecular_formulas.R
@@ -177,6 +177,118 @@ ipMF <- function(mz,
   return(mfs)
 }
 
+#' Molecular formula adduct transformation
+#' @description adduct transform a molecular formula.
+#' @param MF molecular formula to transform.
+#' @param adduct adduct to use for transformation.
+#' @param adduct_rules_table Adduct formation rules table to use. Defaults to `adduct_rules()`.
+#' @examples 
+#' adductTransformMF('C6H12O6','[M+H]1+')
+#' @importFrom CHNOSZ count.elements
+#' @export
+
+adductTransformMF <- function(MF,adduct,adduct_rules_table = adduct_rules()){
+  
+  checkAdductTable(adduct_rules_table)
+  
+  tMF <- function(freq,trans,expres){
+    if (!is.na(trans)) {
+      expAt <- trans %>% count.elements()
+      if (F %in% (names(expAt) %in% names(freq))) {
+        tmp <- rep(0,length(which(!(names(expAt) %in% names(freq)))))
+        names(tmp) <- names(expAt)[!(names(expAt) %in% names(freq))]
+        freq <- c(freq,tmp)
+        freq <- freq[order(names(freq))]
+      }
+      if (expres == '+') {
+        freq[names(expAt)] <- freq[names(expAt)] + expAt
+      }
+      if (expres == '-') {
+        freq[names(expAt)] <- freq[names(expAt)] - expAt
+      }
+    }
+    return(freq)
+  }
+  
+  adductRule <- adduct_rules_table %>% 
+    filter(Name == adduct)
+  
+  freq <- MF %>% count.elements()
+  
+  freq <- freq %>% 
+    {. * adductRule$xM} %>%
+    tMF(adductRule$RemAt,'-') %>%
+    tMF(adductRule$AddEx,'+') %>%
+    tMF(adductRule$RemEx,'-')
+  
+  if (T %in% (freq < 0)) {
+    freq <- ''
+  } else {
+    ch <- freq[names(freq) %in% c('C','H')]
+    freq <- freq[!(names(freq) %in% c('C','H'))]
+    freq <- c(ch,freq)
+    freq <- freq[!(freq == 0)]
+    freq[freq == 1] <- ''
+    freq <- str_c(names(freq),freq) %>%
+      str_c(collapse = '') 
+  }
+  
+  return(freq)
+  
+}
+
+#' Transform a molecular formula
+#' @description transform a molecular formula
+#' @param MF molecular formula to transform
+#' @param transformation transformation to apply
+#' @param transformation_rules_table transformations table containing available transformations rules. Defaults to `transformation_rules()`.
+#' @details \code{NA} will be returned if \code{MF} cannot be transformed.
+#' @examples 
+#' transformMF('C4H5O5')
+#' transformMF('C4H5N',transformation = 'M - [OH] + [NH2]')
+#' @importFrom tidyr gather
+#' @importFrom stringr str_c str_replace
+#' @export
+
+transformMF <- function(MF, 
+                        transformation = 'M - [O] + [NH2]', 
+                        transformation_rules_table = transformation_rules()){
+  
+  checkTransformationTable(transformation_rules_table)
+  
+  if (!is.na(transformation)) {
+    elements <- c('C','H','O','N','P','S')
+    
+    transformation <- filter(transformation_rules_table,
+                             `MF Change` == transformation) %>%
+      select(C:S)
+    
+    MF <- count.elements(MF)
+    
+    if (length(which(!(elements %in% names(MF)))) > 0) {
+      MF <- c(MF,rep(0,length(which(!(elements %in% names(MF))))))
+      names(MF)[names(MF) == ''] <- elements[!(elements %in% names(MF))]
+    }
+    
+    MF <- MF[order(names(MF))]
+    MF <- MF + transformation
+    MF <- gather(MF,'Element','Frequency') 
+    
+    if (T %in% (MF$Frequency < 0)) {
+      MF <- NA
+    } else {
+      MF <- MF %>%
+        filter(Frequency > 0)
+      
+      MF$Frequency[MF$Frequency == 1] <- ''
+      
+      MF <- str_c(MF$Element,MF$Frequency) %>%
+        str_c(collapse = '') 
+    }
+  }
+  return(MF)
+}
+
 #' Transformation check
 #' @description Check if a transformation between two molecular formulas is possible
 #' @param from molecular formula to be transformed

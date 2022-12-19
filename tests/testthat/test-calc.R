5bb50cdfc5c6a9b7d55e0b096f68f41fc6f8504b
jasenfinch <jsf9@aber.ac.uk>
2022-12-19 22:04:27 +0000

    increase test coverage

--- tests/testthat/test-calc.R
+++ tests/testthat/test-calc.R
@@ -3,15 +3,20 @@ context('calculation tools')
 
 test_that('calcAccurateMass works',{
   am <- calcAccurateMass('C4H5O5',charge = 0)  
-  expect_true(am == 133.0137)
+  expect_equal(am,133.0137)
+})
+
+test_that('calculate accurate mass with charge != 0',{
+  am <- calcAccurateMass('C4H5O5',charge = 1)  
+  expect_equal(am,133.01315)
 })
 
 test_that('calcM works',{
   M <- calcM(118.08626,adduct = '[M+H]1+',isotope = '13C',transformation = 'M - [O] + [NH2]')
-  expect_true(M == 116.05182)
+  expect_equal(M,116.05182)
 })
 
 test_that('calcMZ works',{
   mz <- calcMZ(116.05182,adduct = '[M+H]1+',isotope = '13C',transformation = 'M - [O] + [NH2]')
-  expect_true(mz == 118.08626)
+  expect_equal(mz,118.08626)
 })
